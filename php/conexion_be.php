9822b09bf4071063f94aad1e2da1728c27ceddd8
Mrmimme <polivaress@miumg.edu.gt>
2023-09-27 13:23:38 -0600

    Probando otra vez la BD

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -2,24 +2,23 @@
     /*$conexion=mysqli_connect("bd-pruebasdesoftware.database.windows.net","master","Somoskudasai17","bd_Electronica");  
     mysqli_set_charset($conexion, "utf8");
     */
-
-    $host = 'bd-pruebasdesoftware.database.windows.net';
-$username = 'master';
-$password = 'Somoskudasai17';
-$db_name = 'bd_Electronica';
-
-//Initializes MySQLi
-$conn = mysqli_init();
-
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
-// Establish the connection
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
-
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+    $serverName = "bd-pruebasdesoftware.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "bd_Electronica",
+        "Uid" => "master",
+        "PWD" => "Somoskudasai17"
+    );
+    
+    // Establece la conexión
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+    
+    // Realiza consultas, operaciones, etc., con la conexión $conn
+    
+    // Cierra la conexión
+    sqlsrv_close($conn);
 
 ?>
