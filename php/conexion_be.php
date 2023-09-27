f3d159643694bfd720b8388ccf7ea7285276e546
Mrmimme <polivaress@miumg.edu.gt>
2023-09-27 15:40:38 -0600

    Corrigiendo las credenciales

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','php_insertarpdo');
+define('DB_HOST','bd-pruebasdesoftware.database.windows.net');
+define('DB_USER','master');
+define('DB_PASS','Somoskudasai17');
+define('DB_NAME','bd_Electronica');
 
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
