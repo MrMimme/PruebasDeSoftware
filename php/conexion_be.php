f11ca97c4e9fb056e55d8f683cbc5c8be76b6ce2
Mrmimme <polivaress@miumg.edu.gt>
2023-09-27 15:33:16 -0600

    Actualizando el TLS para requisitos minimos del servidor

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,18 +1,16 @@
 <?php
-// DB CREDENCIALES DE USUARIO.
-define('DB_HOST','bd-pruebasdesoftware.database.windows.net');
-define('DB_USER','master');
-define('DB_PASS','Somoskudasai17');
-define('DB_NAME','bd_Electronica');
-// Ahora, establecemos la conexión.
-try
-{
-// Ejecutamos las variables y aplicamos UTF8
-$connect = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,
-array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','php_insertarpdo');
+
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Puedes configurar esto según tus necesidades de seguridad.
+    'ssl_version' => 'TLSv1.2' // Especifica la versión de TLS aquí
+);
+
+$connect = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS, $options);
+
 ?>
