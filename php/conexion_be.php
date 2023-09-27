95e290b72c3b86c2aa2cf05f810b75ba5665eca3
Mrmimme <polivaress@miumg.edu.gt>
2023-09-27 14:12:31 -0600

    Probando TLS

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,18 +1,5 @@
 <?php
-    $conexion=mysqli_connect("bd-pruebasdesoftware.database.windows.net","master","Somoskudasai17","bd_Electronica");  
-    mysqli_set_charset($conexion, "utf8");
-    
-    // Importar el controlador de PHP para SQL Server
-/*
-    $server = "bd-pruebasdesoftware.database.windows.net";
-    $database = "bd_Electronica";
-    $username = "master";
-    $password = "Somoskudasai17";
-
-   
-
-// Crear una instancia del controlador
-$conn = new PDO("sqlsrv:Server=$server;Database=$database", $username, $password);
-*/
-
+$conexion=mysqli_connect("bd-pruebasdesoftware.database.windows.net","master","Somoskudasai17","bd_Electronica", array(
+        'tls_version' => 'TLSv1.2'
+    ));
 ?>
