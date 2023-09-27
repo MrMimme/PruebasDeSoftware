73eb058dced6a558ec0bcef123f9e4a7f979d107
Mrmimme <polivaress@miumg.edu.gt>
2023-09-27 13:56:59 -0600

    PDO prueba 2

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -34,32 +34,41 @@
     }*/
 
 
-    session_start();
+   //Inicializa la sesion y manda a llamar la conexion
+   session_start();
+   require_once 'conexion_be.php';
 
-include 'conexion_be.php';
+   //Recibe los valores por metodo POST y encripta la contraseña para compara en la BD
+   $correo = $_POST['correo'];
+   $contrasena = $_POST['contrasena'];
 
-$correo = $_POST['correo'];
-$contrasena = $_POST['contrasena'];
+   //Realiza la consulta a la base de datos
+   $consulta = $conexion->prepare("SELECT * FROM usuarios WHERE correo = :correo AND contrasena = :contrasena");
+   $consulta->bindParam(':correo', $correo);
+   $consulta->bindParam(':contrasena', $contrasena);
+   $consulta->execute();
 
-// Prepara la consulta SQL
-$query = "SELECT * FROM usuarios WHERE correo = :correo AND contrasena = :contrasena";
-$stmt = $conexion->prepare($query);
-$stmt->bindParam(':correo', $correo);
-$stmt->bindParam(':contrasena', $contrasena);
-$stmt->execute();
+   //Verifica si hay resultados
+   if ($consulta->rowCount() > 0) {
 
-if ($stmt->rowCount() > 0) {
-    $_SESSION['Usuario'] = $correo;
-    header("location: ../HomePage.php");
-    exit;
-} else {
-    echo '
-        <script>
-            alert("Usuario no existe, por favor verifique los datos introducidos");
-            window.location = "../index.php";
-        </script>';
-    exit;
-}
+       //Crea la sesión del usuario
+       $_SESSION['Usuario'] = $correo;
+
+       //Redirecciona al usuario a la página principal
+       header("location: ../HomePage.php");
+       exit;
+
+   } else {
+
+       //Muestra un mensaje de error
+       echo '
+               <script>
+                   alert("Usuario no existe por favor verifique los datos introduciodos");
+                   window.location = "../index.php";
+               </script>    
+       ';
+       exit;
+   }
 
 
 ?>
