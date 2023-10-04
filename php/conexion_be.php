f42c237e3cc593f050a9678ad46175a22d47033b
Mrmimme <polivaress@miumg.edu.gt>
2023-10-04 16:39:16 -0600

    Probando con bard

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,15 +1,15 @@
 <?php
-/*
+
 // Define las variables de conexión a la base de datos
 $hostname = "pruebas-de-software.mysql.database.azure.com";
 $username = "master";
 $password = "Somoskudasai17";
-$database = "bd_Electronica";
+$database = "bd_electronica";
 
 // Realiza la conexión a la base de datos
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "ssl/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL) or die("Error al conectar" . mysqli_error());
+mysqli_ssl_set($conn, NULL, NULL, "../ssl/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, $hostname, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL) or die("Error al conectar" . mysqli_error($conn));
 
 // Comprueba si la conexión es correcta
 if (!$conn) {
@@ -17,13 +17,14 @@ if (!$conn) {
 }
 
 // Cierra la conexión a la base de datos
-mysqli_close($conn);*/
+mysqli_close($conn);
 
-
+/*
 $conexion=mysqli_init();
-mysqli_ssl_set($conexion,NULL,NULL,"ssl/BaltimoreCyberTrustRoot.crt.pem",NULL,NULL);
+mysqli_ssl_set($conexion,NULL,NULL,"../ssl/BaltimoreCyberTrustRoot.crt.pem",NULL,NULL);
 mysqli_real_connect($conexion, "pruebas-de-software.mysql.database.azure.com","master","Somoskudasai17","bd_electronica",3306,
       MYSQLI_CLIENT_SSL) or die ("Error al conectar" . mysqli_error($conexion));
+*/
 ?>
 
 
